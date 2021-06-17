b6c32b0c06cb5045ab68c3f6bbac167d126f1dc3
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-17 21:38:39 +0700

    Tugas3 fix

---
+++ Tugas3.js
@@ -0,0 +1,13 @@
+function SeleksiNilai(nilaiAwal, nilaiAkhir, [...arr]) {
+	sortArr = arr.sort((a, b) => a - b);
+	if (nilaiAwal < nilaiAkhir && sortArr.length > 5) {
+		found = sortArr.filter((x) => x > nilaiAwal && x < nilaiAkhir);
+		console.log(found);
+	} else if (sortArr.length > 5) {
+		console.log('Jumlah angka dalam dataArray harus lebih dari 5');
+	} else if (nilaiAwal < nilaiAkhir) {
+		console.log('Nilai akhir harus lebih besar dari nilai awal');
+	}
+}
+
+SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
