086527a35f541e56cef500537ba2759be86814b1
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-17 21:46:23 +0700

    change variable

--- Tugas4.js
+++ Tugas4.js
@@ -3,21 +3,21 @@ function runTimer(time) {
 	newTime = time * 60000 + 1000 + 60000;
 
 	//convert today's date to ms and add newtime
-	var countDownMinutes = new Date().getTime() + newTime;
+	const countDownMinutes = new Date().getTime() + newTime;
 
-	var x = setInterval(function () {
+	const x = setInterval(function () {
 		// get today's date
-		var now = new Date().getTime();
+		let now = new Date().getTime();
 
 		// calculate the distance between countDownminutes and now
-		var distance = countDownMinutes - now;
+		let distance = countDownMinutes - now;
 
 		// Time calculations for minutes and seconds
 
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 		//add 0 in front of numbers
 		newMinutes = ('0' + minutes).slice(-2);
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 		newSeconds = ('0' + seconds).slice(-2);
 
 		// Output the result
@@ -26,4 +26,4 @@ function runTimer(time) {
 	}, 1000);
 }
 
-runTimer(1);
+runTimer(2);
