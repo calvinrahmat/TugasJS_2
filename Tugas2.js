b15636a83371ddce779d17e9fdcf1dbf236edf06
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-20 10:33:21 +0700

    modified to arrow function

--- Tugas2.js
+++ Tugas2.js
@@ -17,33 +17,27 @@ const name = [
 	'Penelope',
 ];
 
-function searchName(char, limit, myCallBack) {
+let searchName = (char, limit, myCallBack) => {
 	//convert char input to lowercase
 	newChar = char.toLowerCase();
 
 	//convert data name to lowecase
-	lowerCase = name.map(function (x) {
-		return x.toLowerCase();
-	});
+	lowerCase = name.map((x) => x.toLowerCase());
 
 	//filter data name with char input
-	filtered = lowerCase.filter(function (key) {
-		return key.includes(newChar);
-	});
+	filtered = lowerCase.filter((key) => key.includes(newChar));
 
 	//limit filtered data to only 3
 	fix = filtered.slice(0, limit);
 
 	//process to function
 	myCallBack();
-}
+};
 
-function callBack() {
+const callBack = () => {
 	//convert data name back to normal (with first character in uppercase)
-	upperCase = fix.map(function (y) {
-		return y.charAt(0).toUpperCase() + y.slice(1);
-	});
+	const upperCase = fix.map((y) => y.charAt(0).toUpperCase() + y.slice(1));
 	console.log(upperCase);
-}
+};
 
 searchName('an', 3, callBack);
